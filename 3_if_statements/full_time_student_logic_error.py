b75c8a1a02630b1c942b479463b2d9a8155b2c8b
clara <me@me.com>
2019-02-06 11:00:24 -0600

    why the order of if and elif matters

---
+++ 3_if_statements/full_time_student_logic_error.py
@@ -0,0 +1,25 @@
+"""
+if, elif, else Enrollment status at Minneapolis College is defined as the following:
+
+Full time: 12 or more credits
+Half time: 6-11 credits
+Less than half time: 1-5 credits
+
+BUT this program doesn't work as expected. What does it print if you enter 7
+
+"""
+
+number_of_credits = int(input('How many credits you are taking this semester? '))
+
+# This if - elif - else statement doesn't work as expected. Can you see why?
+# Try with 7 credits - it doesn't print the right answer.
+
+if number_of_credits >= 6:
+    print('You are a full-time student')
+elif number_of_credits >= 12:
+    print('You are a half-time student')
+else:
+    print('You are less than half-time')
+
+
+
