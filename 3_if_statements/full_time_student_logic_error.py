55a3bb7b07bb81dac42a183ed62066adcccae85a
clara <me@me.com>
2019-02-06 11:15:01 -0600

    full time part time error, error fixed

--- 3_if_statements/full_time_student_logic_error.py
+++ 3_if_statements/full_time_student_logic_error.py
@@ -15,9 +15,9 @@ number_of_credits = int(input('How many credits you are taking this semester? ')
 # Try with 7 credits - it doesn't print the right answer.
 
 if number_of_credits >= 6:
-    print('You are a full-time student')
-elif number_of_credits >= 12:
     print('You are a half-time student')
+elif number_of_credits >= 12:
+    print('You are a full-time student')
 else:
     print('You are less than half-time')
 
