b42aa6e0b03a586e510ea0466972806e3d8c51cf
clara <10088152+claraj@users.noreply.github.com>
2020-08-26 14:18:48 -0500

    testing if a number is in a range

---
+++ 3_if_statements/number_of_credits_in_range.py
@@ -0,0 +1,11 @@
+""" Testing if a number is within a range """
+
+number_of_credits = int(input('Please enter the number of credits you are taking this semester'))
+
+# A valid number of credits must be between 0 and 24.
+# When you run this program, try -10, 0, 10, 24, 100 to test it.
+
+if number_of_credits >= 0 and number_of_credits <= 24:
+    print('Thank you, you are taking ' + str(number_of_credits) + ' credits')
+else:
+    print(str(number_of_credits) + ' is not a valid number of credits')
