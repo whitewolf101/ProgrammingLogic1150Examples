81da3eaf4f7413677de57e7b0e4f550c4902cdfd
Clara <10088152+claraj@users.noreply.github.com>
2021-01-25 15:12:51 -0600

    Update quiz.py

--- 3_if_statements/quiz.py
+++ 3_if_statements/quiz.py
@@ -11,5 +11,7 @@ if answer == 'Madison':
     print('Correct!')
 
 # Problems with this program
-# 1. The answer has to be in the same case as the answer in the program, so "sacramento" is wrong
+# 1. The answer has to be in the same case as the answer in the program, so "madison" is wrong
 # 2. The user doesn't see any message if they get the answer wrong
+
+print('Thanks for taking the quiz!')
