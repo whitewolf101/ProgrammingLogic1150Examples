fa9a01be4c2c667c9705d8dfa1ea7b5456050f6e
clara <10088152+claraj@users.noreply.github.com>
2021-03-03 10:56:58 -0600

    comments

--- 6_lists/multiple_choice_quiz.py
+++ 6_lists/multiple_choice_quiz.py
@@ -27,24 +27,33 @@ def main():
 
 
 def ask_question(question, answers):
-    # The correct answer is the first answer in the answers list
+    # The correct answer is the first answer in the answers list,
+    # save the text of the answer in a new variable
     correct_answer = answers[0]
+
+    # Shuffle the answers into a random order
     random.shuffle(answers)
 
     print()  # blank line
 
+    # Ask the questions
     print(question)
 
+    # Print the choices, each labeled with a number
     for index, answer in enumerate(answers):
         print(f'{index}: {answer}')
 
+    # User will enter the number for their chosen answer
     user_answer_number = int(input('Enter the number for your answer: '))
 
     # check that the choice is a number between 0 and the maximum list index
     while user_answer_number < 0 or user_answer_number > len(answers) - 1:
         user_answer_number = int(input('Not a valid choice, Enter the number for your answer: '))
 
-    if answers[user_answer_number] == correct_answer:
+    # Read the text of the user's answer from the list
+    user_answer = answers[user_answer_number]
+
+    if user_answer == correct_answer:
         print('Correct!')
     else:
         print(f'Sorry, the correct answer is {correct_answer}')
